369337b54c618d42c3b99b40a4ebc7eb091d7afa
Tristan Hall <tristanalex1986@gmail.com>
2018-07-14 09:46:53 +0100

    mapped out rythm for main guitar hook

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@ $(() => {
   // 4. alert a collission in the console
 
 
+  // create each song in notation vs 1/4s vs a/b/c/d key strokes **** needs a few hours
+
+  // 250ms countdowns through 4 arrays to trigger through 1/0
+
+const a = [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]; // this will be ENTIRE SONG button 1 commands
+const b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // this will be ENTIRE SONG button 2 commands
+const c = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // this will be ENTIRE SONG button 3 commands
+const d = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // this will be ENTIRE SONG button 4 commands
+
+  // arrange in arrays and upload to the hub for checking
+
 
   const note = document.querySelector('.note');
 
@@ -17,19 +28,13 @@ $(() => {
       {transform: 'translateY(0px)'},
       {transform: 'translateY(830px)'}
     ], {
-      duration: 5000,
+      duration: 5000
     });
   };
   moveNote();
   console.log(note.x);
 
 
-  // if (note.x < target.x + target.width &&
-  //  note.x + note.width > target.x &&
-  //  note.y < target.y + target.height &&
-  //  note.height + note.y > target.y) {
-  //   console.log('collission detected');
-  // }
-
+  window.setInterval;
 
 });
